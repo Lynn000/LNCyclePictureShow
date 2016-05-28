0f97b9eacd67f639a94024146a1ba2e22e6a2489
Lynn010 <Lynn000123@hotmail.com>
2016-05-29 07:20:37 +0800

    Initial Commit

---
+++ LNCyclePictureShow/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LNCyclePictureShow
+//
+//  Created by cong on 16/5/29.
+//  Copyright © 2016年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LNCyclePictureShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNCyclePictureShow
+//
+//  Created by cong on 16/5/29.
+//  Copyright © 2016年 lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
